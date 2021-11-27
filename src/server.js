557fcc589017a4f3215ad2f60604d2f6a789e8b5
JWooni <wjddns59@naver.com>
2021-11-27 21:03:15 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -15,6 +15,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
+});
+
 // const wss = new WebSocket.Server({ server });
 // const sockets = [];
 
